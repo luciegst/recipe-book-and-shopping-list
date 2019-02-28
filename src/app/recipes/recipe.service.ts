9c2455345528755ac8c317e1852cee1c2bf9ef8c
luciegenest <lucie.genest.ext@suez.com>
2019-02-28 11:43:22 +0100

    add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
@@ -7,8 +8,24 @@ export class RecipeService {
     // Recipe array based on recipe model //
     //make it private so that you can't directly access this array from outside //
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-    new Recipe('Another test Recipe', 'This is simply a test', 'https://images.media-allrecipes.com/images/56589.png')
+    new Recipe(
+        'Shrimp salad', 
+        'Avocado and shrimp are the best mix ever !', 
+        'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+        [
+            new Ingredient('Shrimp', 10),
+            new Ingredient('Zuchini', 2),
+            new Ingredient('Avocado', 1),
+        ]),
+    new Recipe(
+        'Pasta salad', 
+        'A classic one, but a really good vegetarian idea', 
+        'https://images.media-allrecipes.com/images/56589.png',
+        [
+            new Ingredient('Pasta', 50),
+            new Ingredient('preserved tomatoes', 10),
+            new Ingredient('green asparagus', 15),
+        ])
   ];
 
   getRecipes() {
