116dce2c6a1a7db8807a88b869b9e26be8998c44
luciegenest <lucie.genest.ext@suez.com>
2019-03-04 13:58:18 +0100

    get method to get back recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { RecipeService } from "../recipes/recipe.service";
+import { Response } from "@angular/http";
+import { Recipe } from "../recipes/recipe.model";
+import { map } from 'rxjs/operators';
+
 
 @Injectable()
 export class DataStorageService {
@@ -8,5 +12,27 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put('https://ng-recipe-book-1181b.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get('https://ng-recipe-book-1181b.firebaseio.com/recipes.json')
+    .pipe(map(
+      (response: Response) => {
+        const recipes: Recipe[] = response.json();
+        for (let recipe of recipes) {
+          if (!recipe['ingredients']) {
+            console.log(recipe)
+            recipe['ingredients'] = []
+          }
+        }
+        return recipes
+      }
+    ))
+    .subscribe(
+      (response) => {
+        const recipes: Recipe[] = response;
+        this.recipeService.setRecipes(recipes);
+      }
+    )
   }
 }
